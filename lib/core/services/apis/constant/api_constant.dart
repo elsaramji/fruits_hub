0b2f68021643da776450fe4ac08ebb5ec315a9b6
Mahmoud Ahamed Al-Saramiji <90051301+elsaramji@users.noreply.github.com>
2026-03-26 02:12:25 +0200

    feat: add API constants for base URL and authentication endpoints.

--- lib/core/services/apis/constant/api_constant.dart
+++ lib/core/services/apis/constant/api_constant.dart
@@ -1,6 +1,6 @@
 abstract final class ApiConstant {
 
-static String baseUrl = "http://10.0.2.2:8000";
+static String baseUrl = "https://saramegy-fruits250.hf.space";
 static String loginPath = "/auth/login";
 static String registerPath = "/auth/register";
 static String logoutPath = "/auth/logout";
